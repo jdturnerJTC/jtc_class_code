9065bb753e37053b5ff0b2ac17083046dd80a84a
jdturnerjtc <jdturner00@gmail.com>
2020-09-27 15:39:21 -0400

    add all files

---
+++ class_scripts/bootcamp_scripts/print_exercise.py
@@ -0,0 +1,17 @@
+# this line of code prints the first line of text
+
+print('hi, this is your computer speaking.')
+# this line of code prints the first line of text
+print('I can run a lot of python code')
+
+firstname = 'Paul'
+print(firstname)
+
+print(1+3)
+print(1*3)
+print(1-3)
+
+positive_number = 10
+negative_number = -5
+
+print(positive_number + negative_number)
