2f29a144fb951047349fcf09f9c3b4e44066d6c6
jdturnerjtc <jdturner00@gmail.com>
2020-10-03 17:06:24 -0400

    adding temperature.py challenge

--- 03_primitive_data_types_part_1/temperature.py
+++ 03_primitive_data_types_part_1/temperature.py
@@ -1,19 +1,22 @@
 # challenge 9/22/20
 
-# Convert 100 degrees fahrenheit to celsius
+# 1. Convert 100 degrees fahrenheit to celsius
 celsius_100 = (100 - 32) * (5/9)
 print(celsius_100)
+print('Yes, this is a float.')
 
-# Convert 0 degrees fahrenheit to celsius
+# 2. Convert 0 degrees fahrenheit to celsius
 celsius_0 = (0 - 32) * (5/9)
 print(celsius_0)
 
-# Convert 34.2 degrees fahrenheit to celsius without any variables
-print((34.2-0) *(5/9))
 
-# Convert 5 degrees celsius to fahrenheit 
-print((5+32)/(5/9))
+# 3. Convert 34.2 degrees fahrenheit to celsius without any variables
+print((34.2-32) *(5/9))
 
- # Which is hotter? 30.2 degrees celsius or 85.1 degrees fahrenheit.  Answer = 30.2 degrees celsius
+# 4. Convert 5 degrees celsius to fahrenheit 
+print((5*9/5) + 32)
 
-print((30.2+32)/(5/9))
+ # 5. Which is hotter? 30.2 degrees celsius or 85.1 degrees fahrenheit.  Answer = 30.2 degrees celsius
+
+print((30.2*9/5) + 32)
+print('30.2 degrees celsius is hotter than 85.1 degrees fahrenheit.')
